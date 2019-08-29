bd6f875863650997681858f608b7627bab25ab1c
dwindes24 <daviswindes@gmail.com>
2019-08-29 13:14:29 -0700

    took out some uneccessary comments

--- snakeGame/snakeGame.cpp
+++ snakeGame/snakeGame.cpp
@@ -125,6 +125,8 @@ void Logic()
 	}
 	if (x > boardWidth || x < 0 || y > boardHeight || y < 0)
 		gameOver = true;
+
+	//below can be used if you want to go through the wall and come out the other side
 	//if (x >= boardWidth) x = 0; else if (x < 0) x = boardWidth - 1;
 	//if (y >= boardHeight) y = 0; else if (y < 0) y = boardHeight - 1;
 
@@ -152,13 +154,3 @@ int main()
 	}
 	return 0;
 }
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
